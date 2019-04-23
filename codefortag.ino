e26c50ebdc539c5cc0dab934fb9b14651d4f187f
Abdullah <46641712+aalhasawi@users.noreply.github.com>
2019-04-23 02:58:03 -0700

    Updated to print on OLED and blink built-on LED

--- codefortag.ino
+++ codefortag.ino
@@ -5,9 +5,8 @@
 
 OledWingAdafruit display;
 
-void myHandler(const char *event, const char *data)
 
-{}
+int led2 = 7;
 
 void setup() 
 
@@ -15,21 +14,34 @@ void setup()
 	display.setup();
 	display.clearDisplay();
 	display.display();
-	Particle.subscribe("tag1",myHandler);   //each tag subscribes to its event stream. In this case Tag1 is this guys event
+	Particle.subscribe("tag2",myHandler);   //each tag subscribes to its event stream. In this case Tag1 is this guys event
+	pinMode(led2, OUTPUT);
 }
 
-void loop()
 
-{
+// void loop()
+
+// {}
+
+    void myHandler(const char *event, const char *data)
+    {
+        
+        
+    digitalWrite(led2, HIGH);
+    delay(100);
+    digitalWrite(led2, LOW);
+    delay(100);
     display.loop();
 	display.clearDisplay();
-	display.setTextSize(1);
+	display.setTextSize(1.5);
 	display.setTextColor(WHITE);
 	display.setCursor(0,0);
-	display.println("event=%s data=%s", event, data ? data : "NULL"); //beacon passes the message. Tag prints msg on OLED
+	//display.println(event);
+	display.println(data);
+	 //beacon passes the message. Tag prints msg on OLED
 	//maybe add a button here to clear screen and a light to blink when this thing prints anything.
 	display.display();
-}
-
-
+	
+	
+    }
 //This code is only for one tag. Each tag should have line 18 Particle.subscribe("tag1",myHandler); changed to its event's name.
